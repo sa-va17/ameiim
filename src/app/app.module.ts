3c11085711fb681ac025953205b1cf4fc0601e09
sa-va17 <vsugandha17@gmail.com>
2021-10-12 17:51:57 +0530

    new lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MdbCarouselModule }from 'mdb-angular-ui-kit/carousel';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +17,12 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    MdbCarouselModule,
+    MdbDropdownModule,
+    NgbModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
